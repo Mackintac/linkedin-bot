48491162daebeed59fd013a5e042c43c9e29c95e
Mackintac <mack-robinson@hotmail.ca>
2024-01-29 14:58:44 -0500

    add go.mod, import for godotenv, gitignore, .env

---
+++ go.mod
@@ -0,0 +1,5 @@
+module dev/linkedIn
+
+go 1.20
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	http.ListenAndServe("", nil)
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading information from .env")
+	}
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World!")
+	}
+	http.HandleFunc("/", handler)
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
 
+	fmt.Println("Server live")
+	log.Fatal(server.ListenAndServe())
 }
