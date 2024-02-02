2d8918d4c147c5761a0c423dc3f3c9a1dc8c630d
Mackintac <mack-robinson@hotmail.ca>
2024-02-01 20:25:43 -0500

    messing around with error handling

--- main.go
+++ main.go
@@ -9,18 +9,48 @@ import (
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
+
+	if err := envInit(); err != nil {
 		log.Fatal("Error loading information from .env")
+		return
 	}
+
+	if err := helloHandler(); err != nil {
+		log.Fatal("Error loading Hello handler")
+		return
+	}
+
+	fmt.Println("Server going live")
+	if err := startServer(); err != nil {
+		log.Fatal("Error starting server")
+		return
+	}
+}
+
+func envInit() error {
+	// .env initialization
+	godotenv.Load(".env")
+	return nil
+}
+
+func helloHandler() error {
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, World!")
+		fmt.Println("Server Pinged!")
 	}
 	http.HandleFunc("/", handler)
+	return nil
+}
+
+func startServer() error {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 
-	fmt.Println("Server live")
-	log.Fatal(server.ListenAndServe())
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Server dead")
+		log.Fatal("Error with ListenAndServe")
+	}
+	return nil
 }
