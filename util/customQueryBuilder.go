5f2cf538dce258ec8b4c5bbf7dbab2095300a135
Mackintac <mack-robinson@hotmail.ca>
2024-04-20 20:27:20 -0400

    create a function for giving an rng int based on size of a given struct

--- util/customQueryBuilder.go
+++ util/customQueryBuilder.go
@@ -39,3 +39,23 @@ func CustomQueryBuilder() string {
 
 	return customQuery
 }
+
+func rngForStruct(s struct{}) int {
+
+	sValue := reflect.ValueOf(s)
+	var structSlice []interface{}
+
+	for i := 0; i < sValue.NumField(); i++ {
+		fieldValue := sValue.Field(i).Interface()
+		structSlice = append(structSlice, fieldValue)
+	}
+
+	source := rand.NewSource(time.Now().UnixNano())
+
+	rng := rand.New(source)
+
+	structRng := rng.Intn(len(structSlice) - 1)
+
+	return structRng
+
+}
