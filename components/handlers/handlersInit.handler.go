5b510bb0e440ae3b1c40cc3f9b0e03392f5d6c6e
Mackintac <mack-robinson@hotmail.ca>
2024-04-23 23:51:02 -0400

    create a rudimentary timer that pings the /newShare and /newQuery eps at regular intervals.

--- components/handlers/handlersInit.handler.go
+++ components/handlers/handlersInit.handler.go
@@ -1,6 +1,8 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func InitHandlers() {
 
@@ -9,8 +11,10 @@ func InitHandlers() {
 	newQueryHandler, queryHolderChannel := NewQueryHandler()
 	newShareHandler := NewShareHandler(queryHolderChannel)
 
-	http.HandleFunc(projectConfig.Endpoints.Server.NewShare, newShareHandler)
 	http.HandleFunc(projectConfig.Endpoints.Server.UserInfo, userInfoHandler)
 	http.HandleFunc(projectConfig.Endpoints.Server.NewQuery, newQueryHandler)
+	http.HandleFunc(projectConfig.Endpoints.Server.NewShare, newShareHandler)
 
+	go PostTimer(projectConfig.Endpoints.Server.NewQuery, "query")
+	go PostTimer(projectConfig.Endpoints.Server.NewShare, "share")
 }
---
+++ components/handlers/postTimer.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func PostTimer(ep string, epName string) {
+	ticker := time.NewTicker(9 * time.Hour)
+
+	for range ticker.C {
+		fmt.Println("sending an auto-ping to endpoint: " + epName)
+		resp, err := http.Get("http://localhost:8080" + ep)
+		if err != nil {
+			fmt.Println("Error with auto-ping to endpoint: "+epName, err)
+			continue
+		}
+		fmt.Println("Response status:", resp.Status)
+	}
+}
